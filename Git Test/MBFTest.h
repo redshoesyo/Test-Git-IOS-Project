dfc3ee72873e6aecb64798cb23287f3fc3b95616
Mike Boudreau <mikeboudreau@Mikes-MacBook-Air.local>
2014-07-26 17:10:01 -0400

    added a test class

---
+++ Git Test/MBFTest.h
@@ -0,0 +1,13 @@
+//
+//  MBFTest.h
+//  Git Test
+//
+//  Created by Mike Boudreau on 7/26/14.
+//  Copyright (c) 2014 Mike Boudreau. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBFTest : NSObject
+
+@end
---
+++ Git Test/MBFTest.m
@@ -0,0 +1,13 @@
+//
+//  MBFTest.m
+//  Git Test
+//
+//  Created by Mike Boudreau on 7/26/14.
+//  Copyright (c) 2014 Mike Boudreau. All rights reserved.
+//
+
+#import "MBFTest.h"
+
+@implementation MBFTest
+
+@end
